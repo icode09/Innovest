bf12afcaff3f6a46cb2d58771487ab1c5bfd17f0
rohitk2629 <rohitk2629@gmail.com>
2021-07-25 21:41:18 +0530

    added dashboard component, updated routing

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChallengeDescComponent } from './challenge-desc/challenge-desc.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'ch-desc', component:ChallengeDescComponent},
+  {path:'dashboard', component:DashboardComponent},
+  {path:'', redirectTo:'/ch-desc', pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
