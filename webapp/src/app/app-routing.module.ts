1d3ae8dfcd20836e927a349d27e3cb53641ae16e
rohitk2629 <rohitk2629@gmail.com>
2021-07-27 01:48:09 +0530

    updated routing module

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -3,15 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { ChallengeDescComponent } from './challenge-desc/challenge-desc.component';
 import { ChallengeListComponent } from './challenge-list/challenge-list.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
+  {path:'home', component:LandingPageComponent},
+  {path:'login', component:LoginComponent},
   {path:'ch-desc', component:ChallengeDescComponent},
   {path:'dashboard', component:DashboardComponent},
   {path:'ch-list',component:ChallengeListComponent},
-  {path:'', redirectTo:'/ch-desc', pathMatch:'full'},
-  {path:'', component:LoginComponent},
+  {path:'', redirectTo:'/home', pathMatch:'full'},
 ];
 
 @NgModule({
