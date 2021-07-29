8689eaf0b4414a1a8c2d994dee5451fc82bea0ff
unknown <aniruddhasurse88@gmail.com>
2021-07-29 22:14:28 +0530

    MinorUpdate In UI

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {path : 'signup',component : SignupComponent},
   { path: '**', component: PageNotFoundComponent }
-  
+
 ];
 
 @NgModule({
