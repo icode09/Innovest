404ac4aab642f50163b965204099430e00b3d36f
Parag More <paragmore306@gmail.com>
2021-07-30 16:25:44 +0530

    added error handler

--- webapp/src/app/create-challenge/create-challenge.component.ts
+++ webapp/src/app/create-challenge/create-challenge.component.ts
@@ -1,29 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { Challenge } from '../challenge';
 import { CreatingchallengeService } from '../creatingchallenge.service';
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-create-challenge',
   templateUrl: './create-challenge.component.html',
-  styleUrls: ['./create-challenge.component.css']
+  styleUrls: ['./create-challenge.component.css'],
 })
 export class CreateChallengeComponent implements OnInit {
+  constructor(private _creatingChallenge: CreatingchallengeService) {}
 
-  constructor(private _creatingChallenge: CreatingchallengeService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  challengeModel = new Challenge(
+    uuidv4(),
+    uuidv4(),
+    'Challenge Name',
+    'Description',
+    '',
+    new Date(),
+    new Date(),
+    '',
+    '',
+    true,
+    '',
+    '',
+    '',
+    '',
+    0,
+    [0, 0],
+    0,
+    0
+  );
 
-  challengeModel = new Challenge( uuidv4(),uuidv4(), "Challenge Name", "Description","", new Date(), new Date(),"","",true,"","","","",0,[0,0],0,0)
-
-  onSubmit(){
+  onSubmit() {
     this.challengeModel.challengeId = uuidv4();
     this.challengeModel.challengerId = uuidv4();
-    this._creatingChallenge.createChallenge(this.challengeModel)
-    .subscribe(
-      data => console.log('Success!', data),
-      error => console.log('Error!', error)
-    )
+    this._creatingChallenge.createChallenge(this.challengeModel).subscribe(
+      (data) => console.log('Success!', data),
+      (error) => console.log('Error!', error)
+    );
   }
 }
