9c45ee60023144bc5c3cd51c04fbece5f4fa357d
Parag More <paragmore306@gmail.com>
2021-08-02 15:04:07 +0530

    Integrated all services

--- registration-service/src/main/java/com/stackroute/registrationservice/model/User.java
+++ registration-service/src/main/java/com/stackroute/registrationservice/model/User.java
@@ -19,4 +19,52 @@ public class User {
     private String password;
     private String[] domain;
     private String bio;
+	public User(UUID userId, String displayName, String email, String password, String[] domain, String bio) {
+		super();
+		this.userId = userId;
+		this.displayName = displayName;
+		this.email = email;
+		this.password = password;
+		this.domain = domain;
+		this.bio = bio;
+	}
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+	public UUID getUserId() {
+		return userId;
+	}
+	public void setUserId(UUID userId) {
+		this.userId = userId;
+	}
+	public String getDisplayName() {
+		return displayName;
+	}
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String[] getDomain() {
+		return domain;
+	}
+	public void setDomain(String[] domain) {
+		this.domain = domain;
+	}
+	public String getBio() {
+		return bio;
+	}
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
 }
--- webapp/src/app/auth-service.service.ts
+++ webapp/src/app/auth-service.service.ts
@@ -4,32 +4,31 @@ import { Observable } from 'rxjs';
 import { AppConstants } from './common/app.constants';
 import { Router } from '@angular/router';
 
-
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
 };
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class AuthServiceService {
+  constructor(private http: HttpClient, private myroute: Router) {}
+  login(data: any): Observable<any> {
+    return this.http.post(AppConstants.login_API, data);
+  }
 
-  constructor(private http:HttpClient, private myroute: Router) { }
-  login(data:any):Observable<any>{ 
-    return this.http.post(AppConstants.login_API,data)
-}
-
-register(user:any): Observable<any> {
-  return this.http.post('http://localhost:8100/api/v1/register/registered', user);
-}
-storeToken(token:any,username:any){
-  localStorage.setItem("mytoken",token);
-  localStorage.setItem("currently logged in user",username);
-  //sessionStorage.setItem("mytoken",token);
-}
-opendashboard(){
-  this.myroute.navigate(['dashboard']);
-
-}
+  register(user: any): Observable<any> {
+    return this.http.post(
+      'http://localhost:9090/api/v1/register/registered',
+      user
+    );
+  }
+  storeToken(token: any, username: any) {
+    localStorage.setItem('mytoken', token);
+    localStorage.setItem('currently logged in user', username);
+    //sessionStorage.setItem("mytoken",token);
+  }
+  opendashboard() {
+    this.myroute.navigate(['dashboard']);
+  }
 }
