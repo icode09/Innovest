523277026f6d60fd4436b7dea7ab0d4ebfec962b
Parag More <paragmore306@gmail.com>
2021-08-04 09:35:20 +0530

    Working on Mongo Template

--- Solution/src/main/java/com/innovest/solution/controller/SolutionController.java
+++ Solution/src/main/java/com/innovest/solution/controller/SolutionController.java
@@ -7,12 +7,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.innovest.solution.model.Solution;
 import com.innovest.solution.model.SolutionStatus;
@@ -40,5 +35,9 @@ public class SolutionController {
 	@GetMapping("/listall")
 	public ResponseEntity<List<Solution>> getAllSolutions(){
 		return new ResponseEntity<List<Solution>>(solutionService.getAllSolutions(), HttpStatus.OK);
+	}
+	@GetMapping("/challenge")
+	public ResponseEntity<List<Solution>> getSolutionsById(@RequestParam String challengeId){
+		return new ResponseEntity<List<Solution>>(solutionService.getSolutionsByChallenge(UUID.fromString(challengeId)), HttpStatus.OK);
 	}
 }
