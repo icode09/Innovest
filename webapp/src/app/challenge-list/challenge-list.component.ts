b9b6b3f32f19e2b28b85b44c7bc994a893732b9c
rohitk2629 <rohitk2629@gmail.com>
2021-08-09 13:35:35 +0530

    updated dashboard

--- webapp/src/app/challenge-list/challenge-list.component.ts
+++ webapp/src/app/challenge-list/challenge-list.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Challenge } from '../common/challenge';
 import { SearchService } from '../search.service';
-
 import { Observable } from 'rxjs';
 
 
