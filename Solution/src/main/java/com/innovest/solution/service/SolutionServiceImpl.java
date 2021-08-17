2a4f55a91df1f91d04ee3b7de1b0f190f460231e
Get Authorized From Dev <Prashanth.P@stackroute.com>
2021-08-17 16:10:29 +0530

    solution service changes

--- Solution/src/main/java/com/innovest/solution/service/SolutionServiceImpl.java
+++ Solution/src/main/java/com/innovest/solution/service/SolutionServiceImpl.java
@@ -35,95 +35,17 @@ import javax.annotation.PostConstruct;
 @Service
 public class SolutionServiceImpl implements SolutionService {
 
-	SolutionRepository repo;
-	private AmazonS3 s3client;
+    SolutionRepository repo;
+    private AmazonS3 s3client;
 
-	@Autowired
-	public SolutionServiceImpl(SolutionRepository repo) {
-		super();
-		this.repo=repo;
-	}
-	@Autowired
-	private MongoTemplate mongoTemplate;
+    @Autowired
+    public SolutionServiceImpl(SolutionRepository repo) {
+        super();
+        this.repo = repo;
+    }
 
-	@Override
-	public Solution addSolution(Solution solution) {
-		return repo.save(solution);
-	}
-
-	@Override
-	public Solution updateSolution(Solution solution) {
-		Solution toUpdate = repo.findBySolutionId(solution.getSolutionId());
-
-		System.out.println(toUpdate);
-		toUpdate.setCodeUrl(solution.getCodeUrl());
-		toUpdate.setDocumentUrl(solution.getDocumentUrl());
-		toUpdate.setSolutionDescription(solution.getSolutionDescription());
-		toUpdate.setSolutionTitle(solution.getSolutionTitle());
-		toUpdate.setSolutionStatus(solution.getSolutionStatus());
-		return repo.save(toUpdate);
-	}
-
-	@Override
-	public Solution updateSolutionStatus(UUID solutionId, SolutionStatus solutionStatus) {
-		Query query=new Query();
-		Update update= new Update().set("solutionStatus", solutionStatus);
-		query.addCriteria(Criteria.where("solutionId").is(solutionId));
-
-		Solution solution = mongoTemplate.findAndModify(query,update,Solution.class);
-		return solution;
-	}
-
-	@Override
-	public Solution updateReviewComments(UUID solutionId, String[] reviewComments) {
-		Query query=new Query();
-		Update update= new Update().set("reviewComments", reviewComments);
-		query.addCriteria(Criteria.where("solutionId").is(solutionId));
-
-		Solution solution = mongoTemplate.findAndModify(query,update,Solution.class);
-		return solution;
-	}
-
-	@Override
-	public List<Solution> getAllSolutions() {
-		System.out.println(repo.findAll());
-		return (List<Solution>) repo.findAll();
-	}
-
-
-	@Override
-	public List<Solution> getSolutionsBySolutionStatus(SolutionStatus solutionStatus) {
-		// TODO Auto-generated method stub
-		Query query=new Query();
-		query.addCriteria(Criteria.where("solutionStatus").is(solutionStatus));
-		List<Solution> solutions=mongoTemplate.find(query,Solution.class);
-		return solutions;
-	}
-
-	@Override
-	public List<Solution> getSolutionsByChallenge(UUID challengeId) {
-		// TODO Auto-generated method stub
-		Query query=new Query();
-		query.addCriteria(Criteria.where("challengeId").is(challengeId));
-		List<Solution> solutions=mongoTemplate.find(query,Solution.class);
-		return solutions;
-	}
-
-	@Override
-	public List<Solution> getSolutionsByUser(String solvedBy) {
-		// TODO Auto-generated method stub
-		Query query=new Query();
-		query.addCriteria(Criteria.where("solvedBy").is(solvedBy));
-		List<Solution> solutions=mongoTemplate.find(query,Solution.class);
-		return solutions;
-	}
-
-	@Override
-	public void removeSolution(UUID solutionId) {
-		// TODO Auto-generated method stub
-		repo.deleteById(solutionId);
-	}
-
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
 	@Value("${aws.s3.bucket}")
 	private String bucketName;
@@ -146,47 +68,123 @@ public class SolutionServiceImpl implements SolutionService {
 				.build();
 	}
 
-	@Override
-	@Async
-	public String uploadFile(MultipartFile multipartFile) {
-		String fileUrl = "";
-		try {
-			final File file = convertMultiPartFileToFile(multipartFile);
-			String fileName = generateFileName(multipartFile);
-			fileUrl = endpointUrl + "/" + bucketName + "/" + fileName;
-			uploadFileToS3Bucket(fileName, file);
-			file.delete();    // To remove the file locally created in the project folder.
-		} catch (final AmazonServiceException ex) {
-		}
-		return fileUrl;
-	}
+    @Override
+    public Solution addSolution(Solution solution) {
+        return repo.save(solution);
+    }
+
+    @Override
+    public Solution updateSolution(Solution solution) {
+        Solution toUpdate = repo.findBySolutionId(solution.getSolutionId());
+
+        System.out.println(toUpdate);
+        toUpdate.setCodeUrl(solution.getCodeUrl());
+        toUpdate.setDocumentUrl(solution.getDocumentUrl());
+        toUpdate.setSolutionDescription(solution.getSolutionDescription());
+        toUpdate.setSolutionTitle(solution.getSolutionTitle());
+        toUpdate.setSolutionStatus(solution.getSolutionStatus());
+        return repo.save(toUpdate);
+    }
+
+    @Override
+    public Solution updateSolutionStatus(UUID solutionId, SolutionStatus solutionStatus) {
+        Query query = new Query();
+        Update update = new Update().set("solutionStatus", solutionStatus);
+        query.addCriteria(Criteria.where("solutionId").is(solutionId));
+
+        Solution solution = mongoTemplate.findAndModify(query, update, Solution.class);
+        return solution;
+    }
+
+    @Override
+    public Solution updateReviewComments(UUID solutionId, String[] reviewComments) {
+        Query query = new Query();
+        Update update = new Update().set("reviewComments", reviewComments);
+        query.addCriteria(Criteria.where("solutionId").is(solutionId));
+
+        Solution solution = mongoTemplate.findAndModify(query, update, Solution.class);
+        return solution;
+    }
+
+    @Override
+    public List<Solution> getAllSolutions() {
+        System.out.println(repo.findAll());
+        return (List<Solution>) repo.findAll();
+    }
 
 
-	private String generateFileName(MultipartFile multiPart) {
-		return new Date().getTime() + "-" + multiPart.getOriginalFilename()
-				.replace(" ", "_");
-	}
-	private File convertMultiPartFileToFile(final MultipartFile multipartFile) {
-		final File file = new File(multipartFile.getOriginalFilename());
-		try (final FileOutputStream outputStream = new FileOutputStream(file)) {
-			outputStream.write(multipartFile.getBytes());
-		} catch (final IOException ex) {
+    @Override
+    public List<Solution> getSolutionsBySolutionStatus(SolutionStatus solutionStatus) {
+        // TODO Auto-generated method stub
+        Query query = new Query();
+        query.addCriteria(Criteria.where("solutionStatus").is(solutionStatus));
+        List<Solution> solutions = mongoTemplate.find(query, Solution.class);
+        return solutions;
+    }
 
-		} catch (Exception exception) {
+    @Override
+    public List<Solution> getSolutionsByChallenge(UUID challengeId) {
+        // TODO Auto-generated method stub
+        Query query = new Query();
+        query.addCriteria(Criteria.where("challengeId").is(challengeId));
+        List<Solution> solutions = mongoTemplate.find(query, Solution.class);
+        return solutions;
+    }
 
-		}
-		return file;
-	}
-	private void uploadFileToS3Bucket(final String fileName, final File file) {
-		try {
+    @Override
+    public List<Solution> getSolutionsByUser(String solvedBy) {
+        // TODO Auto-generated method stub
+        Query query = new Query();
+        query.addCriteria(Criteria.where("solvedBy").is(solvedBy));
+        List<Solution> solutions = mongoTemplate.find(query, Solution.class);
+        return solutions;
+    }
 
-			final PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, fileName, file);
+    @Override
+    public void removeSolution(UUID solutionId) {
+        // TODO Auto-generated method stub
+        repo.deleteById(solutionId);
+    }
 
-			s3client.putObject(putObjectRequest);
+    @Override
+    @Async
+    public String uploadFile(MultipartFile multipartFile) {
+        String fileUrl = "";
+        try {
+            final File file = convertMultiPartFileToFile(multipartFile);
+            String fileName = generateFileName(multipartFile);
+            fileUrl = endpointUrl + "/" + bucketName + "/" + fileName;
+            uploadFileToS3Bucket(fileName, file);
+            file.delete();    // To remove the file locally created in the project folder.
+        } catch (final AmazonServiceException ex) {
+        }
+        return fileUrl;
+    }
 
-		} catch (Exception exc) {
 
-		}
-	}
-	
+    private String generateFileName(MultipartFile multiPart) {
+        return new Date().getTime() + "-" + multiPart.getOriginalFilename()
+                .replace(" ", "_");
+    }
+
+    private File convertMultiPartFileToFile(final MultipartFile multipartFile) {
+        final File file = new File(multipartFile.getOriginalFilename());
+        try (final FileOutputStream outputStream = new FileOutputStream(file)) {
+            outputStream.write(multipartFile.getBytes());
+        } catch (final IOException ex) {
+
+        } catch (Exception exception) {
+
+        }
+        return file;
+    }
+
+    private void uploadFileToS3Bucket(final String fileName, final File file) {
+        try {
+            final PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, fileName, file);
+            s3client.putObject(putObjectRequest);
+        } catch (Exception exc) {
+        }
+    }
+
 }
