daecef49d84e3f11d9eca22eb8e074c478ebfcda
sankeerth.17je003574 <sankeerth.17je003574@ese.ism.ac.in>
2021-08-23 11:38:06 +0530

    Updated ngOnInit in ChallengeList

--- webapp/src/app/challenge-list/challenge-list.component.ts
+++ webapp/src/app/challenge-list/challenge-list.component.ts
@@ -55,13 +55,15 @@ export class ChallengeListComponent implements OnInit {
     this.url = this.router.url.split('/').pop() || '';
     console.log("Inside ngOnInit");
 
-    this.getChallengeListFromServer();    // getting all the challenges from challenge service
+    
 
     let searchText = localStorage.getItem('searchQuery');
     localStorage.removeItem('searchQuery');
     if(searchText != null){
       this.queries.query = searchText;
       this.searchClick();
+    } else {
+        this.getChallengeListFromServer();    // getting all the challenges from challenge service
     }
     console.log("search:",searchText);
 
